641e8d6b33da0d5b58563d43a049980bf51bd282
istrate-mihai <i.mihai9960@gmail.com>
2026-02-01 17:38:50 +0200

    Fixed profile image responsive behaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,9 +96,13 @@ const Home = {
         `
             <main id="home">
                 <div class="about__me">
-                    <img src="./assets/img/avatar.png" alt="personal-image">
+                    <div id="profile-image-container">
+                        <img src="./assets/img/avatar.png" alt="personal-image" />
+                    </div>
+
                     <h1>Istrate Mihai</h1>
                     <h3>Web Developer</h3>
+
                     <p>Thank you for being here.
                     I am a person passionate about technology and solving problems. I like to contribute to
                     a team of driven people and solve complex issues. I specialize in Fullstack Web Development mostly with Laravel and Vue.js,
